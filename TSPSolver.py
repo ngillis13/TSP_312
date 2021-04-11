3f2c073dc1ffad6bc936c0d997fb2ed44abead18
ngillis13 <64992967+ngillis13@users.noreply.github.com>
2021-04-10 19:06:48 -0600

    Update TSPSolver.py

--- TSPSolver.py
+++ TSPSolver.py
@@ -35,6 +35,18 @@ def findShortestPath(city, remainingCities):
 	return index 
 
 
+
+
+
+def findGroup(city, Groups):
+	for i in range (len(Groups)):
+		tempArray = Groups[i].citiesArray
+		for j in range(len(tempArray)):
+			if (city == tempArray[j]):
+				return Groups[i]
+
+	return None
+
 class subGroup:
 	def __init__(self, citiesArray):
 		self.citiesArray = citiesArray
@@ -42,6 +54,8 @@ class subGroup:
 		self.endNode = None
 		self.pathArray = ([])
 		self.totalDistance = 0
+		self.targetGroup = None
+		self.name = citiesArray[0]._name
 
 
 class TSPSolver:
@@ -195,22 +209,129 @@ class TSPSolver:
 		
 	def fancy( self,time_allowance=60.0 ):
 
+		###############################################################################################################################################################################
+		###############################################################################################################################################################################
+		# PART 1: DALAN
+		###############################################################################################################################################################################
+		###############################################################################################################################################################################
+		
+		###############################################################################################################################################################################
+		# PART 1.1:
 		# we start by taking each city and adding up all the paths connected to that city
 		# then we sort the cities by this total distance with the largest distance being at 
 		# the top of the list
+		###############################################################################################################################################################################
 
+		# Here I initialize a cities list and I initialize a totalDistances list with each of those cities total edge distances
+		cities = self._scenario.getCities()
+		totalDistances = []
+		pairing = {}
+		for i in range (len(cities)):
+			totalDistance = 0
+			for j in range(len(cities)):
+				if (i != j):
+					if (cities[i].costTo(cities[j]) != math.inf):
+						totalDistance += cities[i].costTo(cities[j])
+			pairing[totalDistance] = cities[i]
+			totalDistances.append(totalDistance)
+
+		# Here I am just sorting the cities by distance and storing that in an array called sortedCities
+		# I make a new sorted cities list that will take a sorted version of the distances list and become a list of the corresponding cities to those distances	
+		sortedCities = []
+		totalDistances.sort()
+		for i in range(len(totalDistances)-1,-1,-1):
+			sortedCities.append(pairing[totalDistances[i]])
+
+		# I reverse the list here to make the sortedCities list go from longest total distance to shortest total distance
+		sortedCities.reverse()
+
+		###############################################################################################################################################################################
+		# PART 1.2:
 		# we then take the first city (with largest distance) and sort all the cities it's connected
-		# to (taking out the ones already in a group) by distance starting with the smallest. 
+		# to (taking out the ones already in a group) by distance starting with the sm
+		###############################################################################################################################################################################
 
+		#Here I Initialize a dictionary that will contain each node as a key and as the value it will have a list containing all connected nodes to that key sorted by distance (lowest to highest)
+		sortedEdgeDistances = {}
+		# I loop through each city
+		for i in range(len(sortedCities)):
+			
+			currList = []
+			edgeDistances = {}
+			# I loop through each other city and if the distance isn't infinite, I store the city and length to reference later
+			for j in range(len(sortedCities)):
+				if (i != j):
+					if (sortedCities[i].costTo(sortedCities[j]) != math.inf):
+						edgeDistances[sortedCities[i].costTo(sortedCities[j])] = sortedCities[j]
+						currList.append(sortedCities[i].costTo(sortedCities[j]))
+			currList.sort()
+			currSortedEdgeDistances = []
+			# Here I sort and finalize the edge distances lost for the current node
+			for j in range(len(currList)):
+				currSortedEdgeDistances.append(edgeDistances[currList[j]])
+
+			sortedEdgeDistances[sortedCities[i]] = currSortedEdgeDistances
+
+		###############################################################################################################################################################################
+		# PART 1.3:
 		# Start by adding the next closest node to our starting node. This is the beginning of our group.
 		# To add to the group, we go through the list (of connected nodes sorted by distance) and find the 
 		# node with the shortest cost that has an edge connecting to each of the current nodes in the group
 		# Repeat the process until there are no nodes left that connect to all current nodes in the group and/or cap it at a certain length
 		# Continue the process until each node is in a subgroup
+		###############################################################################################################################################################################
+		
+		# Here I make a list that keeps track of whether each node is in a group already. I initilize all values to false at first
+		inGroup = {}
+		groups = []
+		for i in range(len(sortedCities)):
+			inGroup[sortedCities[i]] = False
+		
+		# I then go through each of the cities (in order of the sortedCities list) and if the city isn't in a group yet, I create a new group and add all valid cities
+		for i in range(len(sortedCities)):
+			
+			# Checking if the current city is in agroup yet
+			if (inGroup[sortedCities[i]] == False):
+
+				# Here I initialize a new group callde currGroup, I add the current city to the group and set the current city as in a group
+				currGroup =[]
+				currGroup.append(sortedCities[i])
+				inGroup[sortedCities[i]] = True
+
+				# I then loop through all of the connected cities to the current node and if they connect to all current cities in the group and aren't in agroup yet I add them to the group
+				for j in range (len(sortedEdgeDistances[sortedCities[i]])):
+					# Here I check if the node I'm looking at is currently in a group
+					if (inGroup[sortedEdgeDistances[sortedCities[i]][j]] == False):
+						isValid = True
+						# Here I check if the node I am looking at connects to all other nodes currently in this group
+						for k in range(len(currGroup)):
+							if (sortedEdgeDistances[sortedCities[i]][j].costTo(currGroup[k]) == math.inf):
+								isValid = False
+						# If it does, I add the node I am looking at to the group and mark it as in a group
+						if (isValid == True):
+							currGroup.append(sortedEdgeDistances[sortedCities[i]][j])
+							inGroup[sortedEdgeDistances[sortedCities[i]][j]] = True
+				groups.append(subGroup(currGroup))
 
 
+		###############################################################################################################################################################################
+		# SUMMARY OF USEFUL VARIABLES:
+		# 
+		# sortedCities: A list containing each city sorted by the total distance of all that city's edges. This list is sorted from highest total distance to lowest total distance
+		# sortedEdgeDistances: A dictionary where the keys are each city (in the same order as sortedCities) and the values are a list containing each connected city to that key node sorted from closest to farthest in terms of cost
+		# groups: A list of lists where each internal list contains all the city objects in that group. Each internal list is one of our final groupings
+		#
+		###############################################################################################################################################################################
 
 
+		###############################################################################################################################################################################
+		###############################################################################################################################################################################
+		# PART 2: NATHAN
+		###############################################################################################################################################################################
+		###############################################################################################################################################################################
+		
+		# Nate's Task:
+		
 		# Sort the subgroups by number of nodes
 
 		# Starting with the subgroup with the fewest nodes, look at all outbound edges leading to nodes in
@@ -225,27 +346,107 @@ class TSPSolver:
 		# each subgroup must have been visited, and a path now exist. there is the chance that not every subgroup
 		# can be visited, but with the penelty included in the cost, we hope to avoid that
 
+		
+		
+		# Sort the subgroups by number of nodes
+
+		groups.sort(key=lambda x: len(x.citiesArray))
+
+		# Starting with the subgroup with the fewest nodes, look at all outbound edges leading to nodes in
+		# different groups (making sure that group hasn't been visited before). Choose the edge with the 
+		# shortest distance, with a penelty for going to larger groups (for instance, times each cost by 1/2 of 
+		# the number of cities in the group). 
 
 
+		# these two arrays are to keep track of nodes we can't make a path to. visitedGroups tells us what groups 
+		# already have a path leading to them. end cities tells us which cities already have a path leading to another
+		# group. in other words, it tracks current end Nodes of all the groups, so we can't go to that city from 
+		# another group/ we can't have the start node and the end node be the same. only exception is for groups
+		# with only one node 
+
+		vistedGroups = []
+		endCities = []
+
+		# here we go through each group for it to find it's path to another group
+		for i in range(len(groups)):
+			min = np.inf
+			pathPair = [None, None]	# will be used to store the path nodes
+			currGroup = groups[i]
+
+			# itterate through the cities in the citiesArray contained in the group object
+			for j in range (len(currGroup.citiesArray)):
+				currCity = currGroup.citiesArray[j]
+				pathOptions = sortedEdgeDistances[currCity]
+
+				# use pathOptions here to itterate through every possible connection from each city in the group
+				for k in range (len(pathOptions)):
+					# use function findGroup to get the group object containg the target node
+					# we are currently testing
+					targetGroup = findGroup(pathOptions[k], groups)
+					
+					# here we need to make sure that the path we are testing isn't: 
+					# 1. to a group that already has a path to it
+					# 2. going back to it's own group
+					# 3. isn't connecting to the end node of the targetGroup (use endCities array) unless only 
+					# one city in group
+					if ((targetGroup not in vistedGroups) 
+					and targetGroup != currGroup 
+					and (pathOptions[k] not in endCities or len(targetGroup.citiesArray) == 1)):
+
+						# here we set the penalty for larger groups, calculate the cost, and if it's lower then	
+						# the current minimum, set it as the new min. we also set the pathPair values to 
+						# the cities we are going from and to, and store what group it will be traversing to  
+						penalty = (len(targetGroup.citiesArray) / 2)
+						cost = currCity.costTo(pathOptions[k])
+						if (cost * penalty < min):
+							
+							 pathPair[0] = currCity	# end node of current group
+							 pathPair[1] = pathOptions[k] # start node of target group
+							 finalTargetGroup = targetGroup
+							 min = cost * penalty
+
+			# once we have been through all possible solutions, we set our group vaiables				
+
+			endCities.append(pathPair[0])	# append exit city to endCities for future use
+			currGroup.endNode = pathPair[0]	# set the end node for our current group
+			currGroup.targetGroup = finalTargetGroup	# set the targetGroup in our current group object
+			finalTargetGroup.startNode = pathPair[1]	# set the start node for the target group
+			vistedGroups.append(finalTargetGroup)	# append the target group to the visited groups array for 
+													# future itterations
+
+		# once this loop comletes each group should have:
+		# 1. a cities array
+		# 2. a start and end node
+		# 3. a target group (mostly for debuging)
+		# they will NOT yet have:
+		# 1. pathArray
+		# 2. totalDistance
+		# use code below for debuging if dessired	
+
+		# for debuging
+		#for i in range (len(groups)):
+		#	print (groups[i].targetGroup.name)
+	
 
 
+		###############################################################################################################################################################################
+
+
+		###############################################################################################################################################################################
+		###############################################################################################################################################################################
+		# PART 3: KIMBALL
+		###############################################################################################################################################################################
+		###############################################################################################################################################################################
 		# now we should have start and end nodes within each subgroup. now, within each subgroup we use a 
 		# algorithm to find the shorest path within the group, starting and ending at the start and end nodes
 		# we include the path as an array within the grouping object
-
+		#
 		# finally, we add the path together, adding the connections from group to group with the path arrays
 		# within the groupings. we also count the cost in the same manner. 
-
-
-		# subgroup_class:
-			# array of cities (actual cities, not just an index)
-			# start city
-			# end city
-			# path_array (actual cities, not just an index)
-			# total distance 
-
-		pass
+		#
+		#
 		
-
+		###############################################################################################################################################################################
+		pass
 
 
